d718bf143ccda0d2a7c7d5b81866d14a928c44d3
KestutisRuockus <kestutisruockus@gmail.com>
2025-02-17 11:18:57 +0200

    Render route name in Navbar component;

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,14 +1,25 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
-  const routeName = pathname.split("/");
+  const searchParams = useSearchParams();
+
+  const routerParts = pathname.split("/").filter(Boolean);
+  const lastPart = routerParts[routerParts.length - 1];
+  const isUpdade = searchParams.get("isUpdate");
+
+  let pageTitle = "Home";
+  if (isUpdade) {
+    pageTitle = "Update";
+  } else if (lastPart) {
+    pageTitle = lastPart;
+  }
 
   return (
     <nav className="w-full px-6 py-2 bg-backgroundSecondary font-700 text-xl text-titleText capitalize">
-      {routeName.length === 2 ? "Home" : routeName[routeName.length - 1]}
+      {pageTitle}
     </nav>
   );
 };
